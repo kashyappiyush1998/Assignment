b55abd23c519132487ba028cabdda4e9735da3a3
Piyush Tiwari <kashyappiyush1998@gmail.com>
2024-11-24 21:57:36 +0530

    interpolate frames

--- run_video.py
+++ run_video.py
@@ -16,7 +16,8 @@ def main(img_path, prompt, out_path):
     pipe.to("cuda")
     prompt = prompt
 
-    border_each_frame = (50, 50, 50, 50)
+    border = 100
+    border_each_frame = (border, border, border, border)
 
     image = Image.open(img_path).convert('RGB')
     mask_frame = Image.new('RGB', image.size)
@@ -25,24 +26,43 @@ def main(img_path, prompt, out_path):
 
     os.makedirs("./frames/", exist_ok=True)
 
-    for i in range(90):
+    for i in range(10):
         image_frame = image_frame.convert('RGB')
         image_frame = ImageOps.expand(image_frame,border=border_each_frame,fill='black')
         out_image = pipe(prompt=prompt, image=image_frame, mask_image=mask_frame).images[0]
-        img_name = f"{i:04d}.png"
+        img_name = f"{i:05d}.png"
         frame_path = os.path.join("./frames/", img_name)
         out_image = out_image.resize(image.size)
         out_image.save(frame_path)
         image_frame = out_image.copy()
 
+    os.makedirs("./interpolated_frames/", exist_ok=True)
+    
+    imgs_path = [os.path.join("./frames/", i) for i in sorted(os.listdir("./frames/"))]
+    total_count = 0
+    for count in range(len(imgs_path)-1):
+        img1 = Image.open(imgs_path[count])
+        img2 = Image.open(imgs_path[count+1])
+        
+        img2 = img2.resize((img1.size[0]+border*2, img1.size[1]+border*2))
+        
+        for i in range(int(border/2)):
+            rect = ((border-i*2), (border-i*2), img2.size[0]-(border-i*2), img2.size[1]-(border-i*2))
+            img = img2.crop(rect)
+            img = img.resize(img1.size)
+            img_name = f"{total_count:05d}.png"
+            img_path = os.path.join("./interpolated_frames/", img_name)
+            img.save(img_path)
+            total_count += 1
+
     (
         ffmpeg
-        .input('/content/frames/*.png', pattern_type='glob', framerate=25)
+        .input('./interpolated_frames/*.png', pattern_type='glob', framerate=25)
         .output(out_path)
         .run()
     )
 
-    os.rmdir("./frames")
+    # os.rmdir("./frames")
 
 
 if __name__ == "__main__":
